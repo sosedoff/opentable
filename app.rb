406517d0a6c14f9c298daa0d5e473c9744a4399a
Dan Sosedoff <dan.sosedoff@gmail.com>
2014-03-23 10:04:14 -0500

    Replace single quotes in app

--- app.rb
+++ app.rb
@@ -1,8 +1,8 @@
-require './boot'
-require 'sinatra'
-require 'sinatra/reloader' if development?
+require "./boot"
+require "sinatra"
+require "sinatra/reloader" if development?
 
-VERSION = '1.1.0'
+VERSION = "1.1.0"
 
 configure do
   set :protection, false
@@ -15,18 +15,18 @@ helpers do
       status: code
     }
 
-    content_type(:json, encoding: 'utf8')
+    content_type(:json, encoding: "utf8")
     halt(code, result.to_json)
   end
 
   def success_response(data)
-    content_type(:json, encoding: 'utf8')
+    content_type(:json, encoding: "utf8")
     data.to_json
   end
 end
 
 before do
-  headers['X-Api-Version'] = VERSION
+  headers["X-Api-Version"] = VERSION
 end
 
 get "/" do
@@ -38,12 +38,12 @@ get "/api/countries" do
   success_response(count: results.size, countries: results)
 end
 
-get '/api/cities' do
+get "/api/cities" do
   results = Restaurant.unique_cities
   success_response(count: results.size, cities: results)
 end
 
-get '/api/restaurants' do
+get "/api/restaurants" do
   if params.empty? 
     error_response("At least one parameter required.")
   end
@@ -63,7 +63,7 @@ get '/api/restaurants' do
   end
 end
 
-get '/api/restaurants/:id' do
+get "/api/restaurants/:id" do
   record = Restaurant.by_id(params[:id])
 
   if record.nil?
