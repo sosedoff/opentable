d8d7de9d4614fe1be74a7ed0a2b82d97b5111976
Dan Sosedoff <dan.sosedoff@gmail.com>
2014-03-23 10:59:50 -0500

    Refactor restaurant presenter

--- app/presenters/restaurant_presenter.rb
+++ app/presenters/restaurant_presenter.rb
@@ -1,24 +1,34 @@
 class RestaurantPresenter
   attr_reader :record, :options
 
-  def initialize(record, options={})
+  def initialize(record, options = {})
     @record  = record
     @options = options
   end
 
   def to_hash
     {
-      'id'                 => record.restaurant_id,
-      'name'               => record.name,
-      'address'            => record.address,
-      'city'               => record.city,
-      'state'              => record.state,
-      'area'               => record.metro_name,
-      'postal_code'        => record.postal_code,
-      'country'            => record.country,
-      'phone'              => record.phone,
-      'reserve_url'        => "http://www.opentable.com/single.aspx?rid=#{record.restaurant_id}",
-      'mobile_reserve_url' => "http://mobile.opentable.com/opentable/?restId=#{record.restaurant_id}",
+      "id"                 => record.restaurant_id,
+      "name"               => record.name,
+      "address"            => record.address,
+      "city"               => record.city,
+      "state"              => record.state,
+      "area"               => record.metro_name,
+      "postal_code"        => record.postal_code,
+      "country"            => record.country,
+      "phone"              => record.phone,
+      "reserve_url"        => reserve_url,
+      "mobile_reserve_url" => mobile_reserve_url
     }
+  end
+
+  private
+
+  def reserve_url
+    "http://www.opentable.com/single.aspx?rid=#{record.restaurant_id}"
+  end
+
+  def mobile_reserve_url
+    "http://mobile.opentable.com/opentable/?restId=#{record.restaurant_id}"
   end
 end
