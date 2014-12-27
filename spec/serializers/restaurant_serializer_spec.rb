338be4ee11e197aaa22b7e04e3217766ae52e617
Dan Sosedoff <dan.sosedoff@gmail.com>
2014-12-27 13:17:17 -0600

    Add restaurant serializer spec

---
+++ spec/serializers/restaurant_serializer_spec.rb
@@ -0,0 +1,29 @@
+require "spec_helper"
+
+describe RestaurantSerializer do
+  let(:restaurant) do
+    double(Fabricate.attributes_for(:restaurant, restaurant_id: "12345"))
+  end
+
+  let(:serializer) do
+    described_class.new(restaurant)
+  end
+
+  describe "to_hash" do
+    it "returns restaurant hash" do
+      expect(serializer.to_hash).to eq ({
+        "id"                 => "12345",
+        "name"               => "Big Cheese Restaurant",
+        "address"            => "123 Cool Street",
+        "city"               => "Chicago",
+        "state"              => "IL",
+        "area"               => "Chicago / Illinois",
+        "postal_code"        => "60657",
+        "country"            => "United States",
+        "phone"              => "(123) 4567890",
+        "reserve_url"        => "http://www.opentable.com/single.aspx?rid=12345",
+        "mobile_reserve_url" => "http://mobile.opentable.com/opentable/?restId=12345"
+      })
+    end
+  end
+end
